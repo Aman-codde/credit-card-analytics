e47cc8e160c1d4ba6b39a054062aa38adb7e6b98
aman-codde <amandeepk221@gmail.com>
2025-05-12 21:11:57 -0700

    feat: setup MongoDB connection with connectToMongoDB and initialize in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectToMongoDB } from './config/db.js';
+import './config/loadEnv.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
+
+//connect to mongodb
+connectToMongoDB();
 
 app.get('/', (req,res) => {
     res.send('Hello World!')
